6676ac8cd6c33b8dba34669af129c0d530b68e7b
Hana-Korea <117172983+Hana-Korea@users.noreply.github.com>
2023-03-03 18:13:59 +0900

    map() 을 이용해 하드코딩 손목보호함...

--- study/src/App.jsx
+++ study/src/App.jsx
@@ -1,23 +1,102 @@
 import { useState } from "react";
+import { useRef } from "react";
+import Menu from "./components/Menu";
+import Handlers from "./components/Handlers";
+import Login from "./components/Login";
 import "./App.css";
 
 function App() {
+  let [커피, 커피변경] = useState(["아메리카노", "돌체라떼", "녹차라떼"]);
+  let [좋아요1, 좋아요변경1] = useState([0, 0, 0]);
   const [count, setCount] = useState(0);
-  const onIncrease = () => {
-    setCount((a) => a + 1);
-  };
-  const onDecrease = () => {
-    setCount((b) => b - 1);
-  };
-  // state변경함수는 딱 한개만 쓸 수 있을 거라고 생각했는데,
-  // 다른 함수안에다 집어넣어서 여러 형태로도 만들 수가 있구나
+  const [coffee, setCoffee] = useState([
+    "아메리카노",
+    "녹차라떼",
+    "바닐라라떼",
+    "홍차",
+    "유자차",
+  ]);
+  const [좋아요, 좋아요변경] = useState([0, 0, 0, 0, 0]);
+
+  const [modal, setModal] = useState(false);
+  function good(x) {
+    let copy1 = [...좋아요];
+    copy1[x] = 좋아요[x] + 1;
+    좋아요변경(copy1);
+  }
+
   return (
     <div className="App">
       <h2>리액트를 공부하는 곳입니다</h2>
-      <button onClick={onIncrease}>+카운트</button>
-      {count}
 
-      <button onClick={onDecrease}>-카운트</button>
+      <div className="coffeeBox">
+        {커피.map(function (c, i) {
+          return (
+            <div>
+              <h4
+                key={i}
+                onClick={() => {
+                  let copy = [...좋아요1];
+                  copy[i] = 좋아요1[i] + 1;
+                  좋아요변경1(copy);
+                }}
+              >
+                {c}
+              </h4>
+              <span>{좋아요1[i]}</span>
+            </div>
+          );
+        })}
+
+        {coffee.map((a, i) => {
+          return (
+            <>
+              <div key={i}>
+                <h4
+                  onClick={() => {
+                    good(i);
+                  }}
+                >
+                  {a}
+                </h4>
+                <span>{좋아요[i]}</span>
+              </div>
+            </>
+          );
+        })}
+
+        {/* onClick={} 중괄호 안에는 함수명을 적든 함수를 새로 만들든 함수자체를 넣어야 한다 */}
+        {/* <button onClick={good}>좋아요버튼</button> */}
+        <button
+          onClick={() => {
+            좋아요변경(좋아요 + 1);
+          }}
+        >
+          좋아요버튼
+        </button>
+        <span>{좋아요}</span>
+      </div>
+
+      <button
+        onClick={() => {
+          setModal(!modal);
+        }}
+      >
+        모달창 생기는 버튼
+      </button>
+
+      {modal == true ? <Modal /> : null}
+    </div>
+  );
+}
+
+function Modal() {
+  const [singer, setSinger] = useState(["아이유", "태연", "백예린"]);
+  return (
+    <div>
+      {singer.map((a, i) => {
+        return <h4 key={i}>{singer[i]} </h4>;
+      })}
     </div>
   );
 }
